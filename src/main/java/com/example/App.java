08b09663bc153b4c8fcaadd2900a992ae97fa16e
aditii <aditiiichoudhary24@gmail.com>
2025-06-10 21:56:09 +0530

    My third Commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,12 @@
-package com.example; 
+package com.example;
 
-public class App { 
-    public int add(int a, int b) { 
-    return a + b; 
-    } 
-public static void main(String[] args) { 
-    System.out.println("2 + 3 = " + new App().add(2,3)); 
-    System.out.println("Application executed 
-    successfully!"); 
-    } 
-} 
+public class App {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("2 + 3 = " + new App().add(2, 3));
+        System.out.println("Application executed successfully!");
+    }
+}
